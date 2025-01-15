9d73487b7736c443f72c00265880c11192fce707
Jain Waldrip <99817486+jain-at-puppet@users.noreply.github.com>
2025-01-15 13:14:24 -0600

    Add PE Primary spec and expand Puppet Agent node spec (#3)
    * Add primary node tests
    
    * Add tests to puppet agent

--- spec/localhost/agent_node_spec.rb
+++ spec/localhost/agent_node_spec.rb
@@ -3,6 +3,13 @@
 require 'spec_helper'
 set :backend, :exec # Set backend to exec for Linux localhost tests
 
-describe package('puppet-agent') do
-  it { is_expected.to be_installed }
+context 'Agent Node' do
+  describe package('puppet-agent') do
+    it { is_expected.to be_installed }
+  end
+
+  describe service('puppet') do
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
+  end
 end
