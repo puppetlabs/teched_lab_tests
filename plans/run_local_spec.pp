d74f3566bd6241c1f51a0447cdfbfb301224faa1
Jain Waldrip <99817486+jain-at-puppet@users.noreply.github.com>
2025-03-06 16:14:59 -0600

    Fix spec runner plan (#5)
    * Add Gitea localhost spec
    
    * Add Bolt tasks to install Ruby and PDK
    
    * Fix install_pdk.sh
    
    * Add localhost test task
    
    * Move pdk bundle install to its own task
    
    * Add plan to run localhost spec
    
    * Use PDK autocorrect
    
    * Remove gitea package from spec - installed manually
    
    * Add failure to run_localhost_test if spec not set
    
    * Add dev environment spec
    
    * Add documentation and fix function call in plan

--- plans/run_local_spec.pp
+++ plans/run_local_spec.pp
@@ -1,20 +1,19 @@
-# This is the structure of a simple plan. To learn more about writing
-# Puppet plans, see the documentation: http://pup.pt/bolt-puppet-plans
-
-# The summary sets the description of the plan that will appear
-# in 'bolt plan show' output. Bolt uses puppet-strings to parse the
-# summary and parameters from the plan.
-# @summary A plan created with bolt plan new.
+# Copyright 2025. Puppet, Inc., a Perforce company.
+#
+# @summary A plan to run spec tests locally on each node.
 # @param targets The targets to run on.
+# @param spec_file The spec file to run.
 plan teched_lab_tests::run_local_spec (
   TargetSpec $targets = 'localhost',
   String $spec_file,                      # A spec file in the spec/localhost/ directory
 ) {
   $spec_dir = 'spec/localhost'
   $spec_path = "${spec_dir}/${spec_file}"
-  if !file_exists($spec_path) {
+
+  if ! file::exists($spec_path) {
     fail("Spec file '${spec_file}' not found in '${spec_dir}'")
   }
+
   run_task('install_ruby', $targets)
   run_task('install_pdk', $targets)
   run_task('bundle_install', $targets)
