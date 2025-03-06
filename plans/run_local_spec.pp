fd500e0d8bec0f8955396db174fb1b98dba659c4
Jain Waldrip <jain.waldrip@perforce.com>
2025-03-06 16:10:50 -0600

    Add documentation and fix function call in plan

--- plans/run_local_spec.pp
+++ plans/run_local_spec.pp
@@ -4,15 +4,16 @@
 # The summary sets the description of the plan that will appear
 # in 'bolt plan show' output. Bolt uses puppet-strings to parse the
 # summary and parameters from the plan.
-# @summary A plan created with bolt plan new.
+# @summary A plan to run spec tests locally on each node.
 # @param targets The targets to run on.
+# @param spec_file The spec file to run.
 plan teched_lab_tests::run_local_spec (
   TargetSpec $targets = 'localhost',
   String $spec_file,                      # A spec file in the spec/localhost/ directory
 ) {
   $spec_dir = 'spec/localhost'
   $spec_path = "${spec_dir}/${spec_file}"
-  if !file_exists($spec_path) {
+  if ! file::exists($spec_path) {
     fail("Spec file '${spec_file}' not found in '${spec_dir}'")
   }
   run_task('install_ruby', $targets)
