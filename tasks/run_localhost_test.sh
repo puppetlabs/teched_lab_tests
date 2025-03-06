2a3ef57a18ebd252d03102fe0b647d7b79da6f57
Jain Waldrip <jain.waldrip@perforce.com>
2025-03-06 14:03:19 -0600

    Add localhost test task

---
+++ tasks/run_localhost_test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Puppet Task Name: run_localhost_test
+# This task runs a test locally from the spec/localhost directory
+if [ -f "spec/localhost/$PT_spec" ]; then
+  echo "Running test $PT_spec"
+  pdk bundle install
+  pdk bundle exec rspec spec/localhost/$PT_spec
+else
+  echo "Test $PT_spec not found"
+  # exit 1
+fi
