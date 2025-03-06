22eedc25fa981fafb78f27503ac53c5068d7527d
Jain Waldrip <jain.waldrip@perforce.com>
2025-03-06 15:45:36 -0600

    Add failure to run_localhost_test if spec not set

--- tasks/run_localhost_test.sh
+++ tasks/run_localhost_test.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 # Puppet Task Name: run_localhost_test
+
+# Exit if spec parameter is not set
+if [ -z "$PT_spec" ]; then
+  echo "Spec parameter is required"
+  exit 1
+fi
+
 # This task runs a test locally from the spec/localhost directory
 if [ -f "spec/localhost/$PT_spec" ]; then
   echo "Running test $PT_spec"
   pdk bundle exec rspec spec/localhost/$PT_spec
 else
   echo "Test $PT_spec not found"
-  # exit 1
+  exit 1
 fi
