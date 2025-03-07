461595eca8fd6a5950e52fd098ac48fd42ef80c1
Jain Waldrip <jain.waldrip@perforce.com>
2025-03-07 10:40:10 -0600

    Fix spec param

--- plans/run_local_spec.pp
+++ plans/run_local_spec.pp
@@ -2,16 +2,16 @@
 #
 # @summary A plan to run spec tests locally on each node.
 # @param targets The targets to run on.
-# @param spec_file The spec file to run.
+# @param spec_file The spec file to run. This must be a file in the spec/localhost directory.
 plan teched_lab_tests::run_local_spec (
   TargetSpec $targets = 'localhost',
-  String $spec_file,                      # A spec file in the spec/localhost/ directory
+  String $spec,                           # A spec file in the spec/localhost/ directory
 ) {
   $spec_dir = 'spec/localhost'
-  $spec_path = "${spec_dir}/${spec_file}"
+  $spec_path = "${spec_dir}/${spec}"
 
   run_task('teched_lab_tests::install_ruby', $targets)
   run_task('teched_lab_tests::install_pdk', $targets)
   run_task('teched_lab_tests::bundle_install', $targets)
-  run_task('teched_lab_tests::run_localhost_test', $targets, 'spec_file' => $spec_file)
+  run_task('teched_lab_tests::run_localhost_test', $targets, 'spec' => $spec)
 }
